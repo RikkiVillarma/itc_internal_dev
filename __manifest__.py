47895538a39d1bd4fd02ef6c08855117593cb9ae
Anicakes1031 <navaljoseph31@gmail.com>
2026-09-02 14:35:29 +0800

    add purchase subsidiary journal

--- __manifest__.py
+++ __manifest__.py
@@ -62,6 +62,7 @@
         'views/purchase_order_lines_views.xml',
         'views/account_move_lines_views.xml',
         'views/collection_receipt_views.xml',
+        'views/purchase_subsidiary_journal_wizard_views.xml',
         # Change Request modules
         #'views/account_payment_register_views.xml',
         #'views/cash_advance_views.xml',
--- wizard/__init__.py
+++ wizard/__init__.py
@@ -1,2 +1,3 @@
 from . import disbursement_voucher_wizard
 from . import purchase_journal_wizard
+from . import purchase_subsidiary_journal_wizard
